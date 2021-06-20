8648c4f5a63787f91b61760217438895c711c51f
Swimming Failure <swimmingfailure@gmail.com>
2021-06-20 12:38:45 -0400

    styling

--- public/swimmingfailure/js/photos.js
+++ public/swimmingfailure/js/photos.js
@@ -1,6 +1,6 @@
 $(function() {
     console.log("PHOTOS");
-    $('.photo').click(function() {
+    $('.image').click(function() {
             url = $(this).attr('data-photo');
             $('#magnified').css('backgroundImage', `url(${url})`);
             $('#overlay').removeClass('hidden');
